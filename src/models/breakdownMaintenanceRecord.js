4928c3d2025ed14e6e2f26ca64c8d21ae0799559
Rupesh Patil <108573358+rupeshpatil02@users.noreply.github.com>
2026-02-14 17:40:35 +0530

    Refactor breakdown maintenance record fields
    Update model and controller to match new maintenance request/work-completion form. The BreakdownMaintenanceRecord model was reworked to replace legacy autoclave/sterilization fields (unique_id, date, material_name, quantity, etc.) with request- and work-oriented fields (from_department, requested_by, request_date/time, equipment_system_area_name, area_id_no, type_of_work_* booleans, description_of_job, request_received_*, change_control_*, summary_of_work_done, work_completed_*, qa_comments, affects_critical_process, equipment_available_for_use, acceptance_user_sign_date, checked_by_engineering_sign_date, verified_by_head_engineering_sign_date). Controller handlers InsertBreakdownMaintenance and EditBreakdownMaintenance were updated to map, create, and update records using the new fields. Also minor formatting changes to association definitions. Note: migrating existing DB data may be required to preserve prior records.

--- src/models/breakdownMaintenanceRecord.js
+++ src/models/breakdownMaintenanceRecord.js
@@ -15,77 +15,117 @@ const BreakdownMaintenanceRecord = sequelize.define(
       allowNull: false,
       references: {
         model: BreakdownMaintenanceForm,
-        key: 'form_id',
-      }
+        key: "form_id",
+      },
     },
-    unique_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    date: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    material_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    quantity: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    autoclave_load_no: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    cycle_no: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    cycle_hold_time: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    autoclave_start_time: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    autoclave_end_time: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    sterilization_hold_start_time: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    sterilization_hold_end_time: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    temperature: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    pressure: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    done_by: {
+
+    from_department: {
       type: DataTypes.STRING,
     },
-    checked_by: {
+    requested_by: {
       type: DataTypes.STRING,
     },
-    reviewed_by: {
+    request_date: {
+      type: DataTypes.DATEONLY,
+    },
+    request_time: {
+      type: DataTypes.TIME,
+    },
+
+    equipment_system_area_name: {
       type: DataTypes.STRING,
     },
-    remarks: {
+    area_id_no: {
       type: DataTypes.STRING,
     },
-  }
+
+    type_of_work_electrical: {
+      type: DataTypes.BOOLEAN,
+    },
+    type_of_work_mechanical: {
+      type: DataTypes.BOOLEAN,
+    },
+    type_of_work_civil: {
+      type: DataTypes.BOOLEAN,
+    },
+    type_of_work_other: {
+      type: DataTypes.BOOLEAN,
+    },
+
+    description_of_job: {
+      type: DataTypes.STRING,
+    },
+
+    user_dept_sign_date: {
+      type: DataTypes.STRING,
+    },
+
+    request_received_by: {
+      type: DataTypes.STRING,
+    },
+    request_received_date: {
+      type: DataTypes.DATEONLY,
+    },
+    request_received_time: {
+      type: DataTypes.TIME,
+    },
+    request_received_sign: {
+      type: DataTypes.STRING,
+    },
+    change_control_required: {
+      type: DataTypes.BOOLEAN,
+    },
+    change_control_no: {
+      type: DataTypes.STRING,
+    },
+
+    summary_of_work_done: {
+      type: DataTypes.STRING,
+    },
+
+    work_completed_by: {
+      type: DataTypes.STRING,
+    },
+    work_completed_date: {
+      type: DataTypes.DATEONLY,
+    },
+    work_completed_time: {
+      type: DataTypes.TIME,
+    },
+    work_completed_sign: {
+      type: DataTypes.STRING,
+    },
+
+    qa_comments: {
+      type: DataTypes.STRING,
+    },
+    affects_critical_process: {
+      type: DataTypes.BOOLEAN,
+    },
+    qa_sign_date: {
+      type: DataTypes.STRING,
+    },
+
+    equipment_available_for_use: {
+      type: DataTypes.BOOLEAN,
+    },
+    acceptance_user_sign_date: {
+      type: DataTypes.STRING,
+    },
+    checked_by_engineering_sign_date: {
+      type: DataTypes.STRING,
+    },
+    verified_by_head_engineering_sign_date: {
+      type: DataTypes.STRING,
+    },
+  },
 );
 
-BreakdownMaintenanceRecord.belongsTo(BreakdownMaintenanceForm, { foreignKey: 'form_id' });
-BreakdownMaintenanceForm.hasMany(BreakdownMaintenanceRecord, { foreignKey: 'form_id' });
+BreakdownMaintenanceRecord.belongsTo(BreakdownMaintenanceForm, {
+  foreignKey: "form_id",
+});
+BreakdownMaintenanceForm.hasMany(BreakdownMaintenanceRecord, {
+  foreignKey: "form_id",
+});
 
 module.exports = BreakdownMaintenanceRecord;
