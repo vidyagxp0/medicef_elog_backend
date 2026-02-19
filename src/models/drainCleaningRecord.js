144f7a324ab4272fbfa665e3009f927b3b2960a9
Yash <yashnamdev1606@gmail.com>
2026-02-19 18:30:15 +0530

    Update drain cleaning record model and controller
    Add new fields to DrainCleaningRecord and adapt EditDrainCleaning to pass them through. Controller now includes by, checked, day, role, row_id, row_label, status and verified_by (removed references to cleaning_agent/disinfectant_used/sanitizer_used). Model was redefined (old definition commented out) to include those new attributes, change time to DATE, set checked as BOOLEAN and use INTEGER for day/row_id. Existing associations between DrainCleaningRecord and DrainCleaningForm are preserved.

--- src/models/drainCleaningRecord.js
+++ src/models/drainCleaningRecord.js
@@ -2,63 +2,121 @@ const { sequelize } = require("../config/db");
 const { DataTypes } = require("sequelize");
 const DrainCleaningForm = require("./drainCleaningForm");
 
-const DrainCleaningRecord = sequelize.define(
-  "DrainCleaningRecord",
-  {
-    record_id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    form_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: DrainCleaningForm,
-        key: 'form_id',
-      }
-    },
-    unique_id: {
-      type: DataTypes.STRING,
-    },
-    date: {
-      type: DataTypes.STRING,
-    },
-    time: {
-      type: DataTypes.STRING,
-    },
-    cleaning_agent: {
-      type: DataTypes.STRING,
-    },
-    disinfectant_used: {
-      type: DataTypes.STRING,
-    },
-    sanitizer_used: {
-      type: DataTypes.STRING,
-    },
-    drain_id: {
-      type: DataTypes.STRING,
-    },
-   
-    done_by: {
-      type: DataTypes.STRING,
-    },
-    checked_by: {
-      type: DataTypes.STRING,
-    },
-    reviewed_by: {
-      type: DataTypes.STRING,
-    },
-    verified_by: {
-      type: DataTypes.STRING,
-    },
-    remarks: {
-      type: DataTypes.STRING,
-    },
-  }
-);
+// const DrainCleaningRecord = sequelize.define(
+//   "DrainCleaningRecord",
+//   {
+//     record_id: {
+//       type: DataTypes.INTEGER,
+//       autoIncrement: true,
+//       primaryKey: true,
+//     },
+//     form_id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       references: {
+//         model: DrainCleaningForm,
+//         key: 'form_id',
+//       }
+//     },
+//     unique_id: {
+//       type: DataTypes.STRING,
+//     },
+//     date: {
+//       type: DataTypes.STRING,
+//     },
+//     time: {
+//       type: DataTypes.STRING,
+//     },
+//     cleaning_agent: {
+//       type: DataTypes.STRING,
+//     },
+//     disinfectant_used: {
+//       type: DataTypes.STRING,
+//     },
+//     sanitizer_used: {
+//       type: DataTypes.STRING,
+//     },
+//     drain_id: {
+//       type: DataTypes.STRING,
+//     },
 
-DrainCleaningRecord.belongsTo(DrainCleaningForm, { foreignKey: 'form_id' });
-DrainCleaningForm.hasMany(DrainCleaningRecord, { foreignKey: 'form_id' });
+//     done_by: {
+//       type: DataTypes.STRING,
+//     },
+//     checked_by: {
+//       type: DataTypes.STRING,
+//     },
+//     reviewed_by: {
+//       type: DataTypes.STRING,
+//     },
+//     verified_by: {
+//       type: DataTypes.STRING,
+//     },
+//     remarks: {
+//       type: DataTypes.STRING,
+//     },
+//   }
+// );
+
+const DrainCleaningRecord = sequelize.define("DrainCleaningRecord", {
+  record_id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  form_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: DrainCleaningForm,
+      key: "form_id",
+    },
+  },
+  unique_id: {
+    type: DataTypes.STRING,
+  },
+  by: {
+    type: DataTypes.STRING,
+  },
+  checked: {
+    type: DataTypes.BOOLEAN,
+  },
+  day: {
+    type: DataTypes.INTEGER,
+  },
+  role: {
+    type: DataTypes.STRING,
+  },
+  row_id: {
+    type: DataTypes.INTEGER,
+  },
+  row_label: {
+    type: DataTypes.STRING,
+  },
+  status: {
+    type: DataTypes.STRING,
+  },
+  time: {
+    type: DataTypes.DATE,
+  },
+
+  done_by: {
+    type: DataTypes.STRING,
+  },
+  checked_by: {
+    type: DataTypes.STRING,
+  },
+  reviewed_by: {
+    type: DataTypes.STRING,
+  },
+  verified_by: {
+    type: DataTypes.STRING,
+  },
+  remarks: {
+    type: DataTypes.STRING,
+  },
+});
+DrainCleaningRecord.belongsTo(DrainCleaningForm, { foreignKey: "form_id" });
+DrainCleaningForm.hasMany(DrainCleaningRecord, { foreignKey: "form_id" });
 
 module.exports = DrainCleaningRecord;
